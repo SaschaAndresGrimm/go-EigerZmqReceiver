efe8d38fce6bd68ff2192e0c2fc8913328b5cc0b
SaschaAndresGrimm <sascha.grimm@hotmail.com>
2019-03-31 10:07:59 +0200

    add windows binary

--- zmqDecoder/bitshuffle/bitshuffle_wrapper.go
+++ zmqDecoder/bitshuffle/bitshuffle_wrapper.go
@@ -1,19 +1,20 @@
 package bitshuffle
 
-// #include <bitshuffle.h>
+// #include "bitshuffle.h"
 import "C"
 
 import (
 	"unsafe"
 )
 
-//BshufDecompressLz4 deflates a bslz4 compressed byte array to an uncompressed byte array.
+//BshufDecompressLz4 inflates a bslz4 compressed byte array to an uncompressed byte array.
+// Returns the number of consumed bytes in input buffer or negative int if error happened.
 func BshufDecompressLz4(in, out unsafe.Pointer, size, elementSize int, blockSize uint32) int {
 	ret := C.bshuf_decompress_lz4(in,
 		out,
-		C.ulong(size),
-		C.ulong(elementSize),
-		C.ulong(blockSize))
+		C.ulonglong(int(size)),
+		C.ulonglong(int(elementSize)),
+		C.ulonglong(int(blockSize)))
 
 	return int(ret)
 
